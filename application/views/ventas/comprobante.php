dc0fb2adfd6e4c7de77a91febcfa6a1dddeee15a
ntizzano <ntizzano>
2025-01-03 22:33:45 -0300

    qrnuevo6

--- application/views/ventas/comprobante.php
+++ application/views/ventas/comprobante.php
@@ -215,8 +215,8 @@ font-size:small;
 							
 							/*<img src="https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=<?=$valor?>">**/
 
-							include "/var/www/html/facturaelectronica/phpqrcode/qrlib.php";
-							QRcode::png($valor,'zapatahs/img/qr'.$venta['id_factura'].'.png', 'L', 4, 2);
+							$this->load->library('phpqrcode/qrlib');
+							$this->qrlib->QRcode->png($valor,'zapatahs/img/qr'.$venta['id_factura'].'.png', 'L', 4, 2);
 							?>
 						</td>
 						<td>
