a330b7c046f5a3eda57b2f6c709cf043ee0f5ee0
ntizzano <ntizzano>
2025-01-03 22:34:55 -0300

    qrnuevo8

--- application/views/ventas/comprobante.php
+++ application/views/ventas/comprobante.php
@@ -216,7 +216,7 @@ font-size:small;
 							/*<img src="https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=<?=$valor?>">**/
 
 							$this->load->library('phpqrcode/qrlib');
-							$this->qrlib->QRcode->png($valor,'zapatahs/img/qr'.$venta['id_factura'].'.png', 'L', 4, 2);
+							$this->QRcode->png($valor,'zapatahs/img/qr'.$venta['id_factura'].'.png', 'L', 4, 2);
 							?>
 						</td>
 						<td>
