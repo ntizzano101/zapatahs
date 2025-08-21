0b4001cd3818528c88dafd85418f283158da2666
ntizzano <ntizzano>
2025-08-21 17:10:08 -0300

    QrOtro

--- application/views/ventas/comprobante.php
+++ application/views/ventas/comprobante.php
@@ -175,6 +175,9 @@ font-size:small;
 	<?php } ?>	
 	<?php 
 	/*recortamos el valor del cae pero da muy largo y no lo toma como numerico*/
+	echo "CAE". substr($venta->cae,1,6);
+	if(is_numeric(substr($venta->cae,1,6))){echo "OKK";}
+	else{echo "NOO";}	
 	if(in_array($venta->letra,array("A","B")) and is_numeric(substr($venta->cae,1,6))) { ?>
 	<tr>
 			<td width="100%" colspan="10" align="center">
